d45a75b289c0efea7c2b4d9c84d565931206df41
vinhhoangd <dhoang@stu.feitian.edu>
2025-06-03 19:02:40 -0400

    Confirm password

--- lib/Pages/signin_page.dart
+++ lib/Pages/signin_page.dart
@@ -1,26 +1,44 @@
 import 'package:flutter/material.dart';
 
-class SignInPage extends StatelessWidget {
-
-
+class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController confirmEmailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  String? emailErrorText;
+
+  void validateEmails() {
+    setState(() {
+      emailErrorText = emailController.text != confirmEmailController.text
+          ? 'Emails do not match'
+          : null;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsetsGeometry.all(20),
+          padding: EdgeInsets.all(20),
           child: Align(
             alignment: Alignment.center,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                //Email text field
-                
+                // Email field
                 TextField(
+                  controller: emailController,
                   decoration: InputDecoration(
                     hintText: 'Email',
+                    errorText: emailErrorText,
                     focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(100),
                       borderSide: BorderSide(
@@ -40,8 +58,36 @@ class SignInPage extends StatelessWidget {
 
                 SizedBox(height: 16),
 
-                //Password text field 
+                // Confirm Email field
                 TextField(
+                  controller: confirmEmailController,
+                  onChanged: (_) => validateEmails(),
+                  decoration: InputDecoration(
+                    hintText: 'Confirm Email',
+                    errorText: emailErrorText,
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100),
+                      borderSide: BorderSide(
+                        color: Colors.greenAccent.shade400,
+                        width: 1,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100),
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 16),
+
+                // Password field
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
                   decoration: InputDecoration(
                     hintText: 'Password',
                     focusedBorder: OutlineInputBorder(
@@ -59,33 +105,37 @@ class SignInPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                  obscureText: true,
                 ),
 
                 SizedBox(height: 32),
 
-                //Sign in button
+                // Sign in button
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.greenAccent.shade400, // button background color
-                    padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12)
+                    backgroundColor: Colors.greenAccent.shade400,
+                    padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
                   ),
-                  onPressed: () => {}, 
+                  onPressed: () {
+                    validateEmails();
+                    if (emailErrorText == null) {
+                      // Proceed with sign in
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("Success!")),
+                      );
+                    }
+                  },
                   child: Text(
-                    'Get Started', 
+                    'Get Started',
                     style: TextStyle(
                       color: Colors.black,
                       fontSize: 16,
                     ),
                   ),
-                )
-
-
+                ),
               ],
             ),
           ),
         ),
-  
       ),
     );
   }
